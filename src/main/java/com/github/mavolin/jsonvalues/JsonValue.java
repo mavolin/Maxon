01e9a2d48041e37b47e1360a5792d41e9bef0283
Maximilian von Lindern <48887425+mavolin@users.noreply.github.com>
2019-07-19 22:14:59 +0200

    added

---
+++ src/main/java/com/github/mavolin/jsonvalues/JsonValue.java
@@ -0,0 +1,26 @@
+package com.github.mavolin.jsonvalues;
+
+/**
+ * The abstract {@code JsonValue} class represents all JSON values.
+ */
+public abstract class JsonValue {
+
+
+    /**
+     * Gets the raw version of the {@code JsonValue}. This will create a short, but not always easy to read String with
+     * the contenets of the respective {@code JsonValue}.
+     *
+     * @return the raw
+     */
+    public abstract String getRaw();
+
+    /**
+     * Gets a pretty printed version of the {@code JsonValue}. While creating the pretty printed version, the focus
+     * is lied on creating a easily readable JSON.
+     *
+     * @return the pretty printed JSON
+     */
+    public abstract String getPrettyPrinted();
+
+
+}
