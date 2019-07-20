afd94388d4138dfb58baf323ad88d33c64b55d81
Maximilian von Lindern <48887425+mavolin@users.noreply.github.com>
2019-07-20 23:59:14 +0200

    Code Cleanup

--- src/main/java/com/github/mavolin/maxon/exceptions/IllegalTypeRequestedException.java
+++ src/main/java/com/github/mavolin/maxon/exceptions/IllegalTypeRequestedException.java
@@ -11,17 +11,15 @@ public class IllegalTypeRequestedException extends RuntimeException {
 
     /**
      * Constructs a new {@code IllegalTypeRequestedException} with {@code null} as its detail message.  The cause is not
-     * initialized, and
-     * may
-     * subsequently be initialized by a call to {@link #initCause}.
+     * initialized, and may subsequently be initialized by a call to {@link #initCause}.
      */
     public IllegalTypeRequestedException() {
 
     }
 
     /**
-     * Constructs a new {@code IllegalTypeRequestedException} with the specified detail message. The cause is not initialized, and may
-     * subsequently be initialized by a call to {@link #initCause}.
+     * Constructs a new {@code IllegalTypeRequestedException} with the specified detail message. The cause is not
+     * initialized, and may subsequently be initialized by a call to {@link #initCause}.
      *
      * @param message
      *         the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
@@ -32,14 +30,15 @@ public class IllegalTypeRequestedException extends RuntimeException {
     }
 
     /**
-     * Constructs a new {@code IllegalTypeRequestedException} with the specified detail message and cause.  <p>Note that the detail message
-     * associated with {@code cause} is <i>not</i> automatically incorporated in this runtime exception's detail message.
+     * Constructs a new {@code IllegalTypeRequestedException} with the specified detail message and cause.  <p>Note that
+     * the detail message associated with {@code cause} is <i>not</i> automatically incorporated in this runtime
+     * exception's detail message.
      *
      * @param message
      *         the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
      * @param cause
-     *         the cause (which is saved for later retrieval by the {@link #getCause()} method).  (A {@code null} value is
-     *         permitted, and indicates that the cause is nonexistent or unknown.)
+     *         the cause (which is saved for later retrieval by the {@link #getCause()} method).  (A {@code null} value
+     *         is permitted, and indicates that the cause is nonexistent or unknown.)
      *
      * @since 1.4
      */
@@ -49,13 +48,14 @@ public class IllegalTypeRequestedException extends RuntimeException {
     }
 
     /**
-     * Constructs a new {@code IllegalTypeRequestedException} with the specified cause and a detail message of {@code (cause==null ? null :
-     * cause.toString())} (which typically contains the class and detail message of {@code cause}).  This constructor is
-     * useful for runtime exceptions that are little more than wrappers for other throwables.
+     * Constructs a new {@code IllegalTypeRequestedException} with the specified cause and a detail message of {@code
+     * (cause==null ? null : cause.toString())} (which typically contains the class and detail message of {@code
+     * cause}).  This constructor is useful for runtime exceptions that are little more than wrappers for other
+     * throwables.
      *
      * @param cause
-     *         the cause (which is saved for later retrieval by the {@link #getCause()} method).  (A {@code null} value is
-     *         permitted, and indicates that the cause is nonexistent or unknown.)
+     *         the cause (which is saved for later retrieval by the {@link #getCause()} method).  (A {@code null} value
+     *         is permitted, and indicates that the cause is nonexistent or unknown.)
      *
      * @since 1.4
      */
@@ -65,8 +65,8 @@ public class IllegalTypeRequestedException extends RuntimeException {
     }
 
     /**
-     * Constructs a new {@code IllegalTypeRequestedException} with the specified detail message, cause, suppression enabled or disabled, and
-     * writable stack trace enabled or disabled.
+     * Constructs a new {@code IllegalTypeRequestedException} with the specified detail message, cause, suppression
+     * enabled or disabled, and writable stack trace enabled or disabled.
      *
      * @param message
      *         the detail message.
