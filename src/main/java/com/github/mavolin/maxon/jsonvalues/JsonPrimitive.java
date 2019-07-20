48b38a303e321b4036838f566b15a59f3d51c0e4
Maximilian von Lindern <48887425+mavolin@users.noreply.github.com>
2019-07-20 18:04:47 +0200

    added logic to constructors; JavaDoc

--- src/main/java/com/github/mavolin/maxon/jsonvalues/JsonPrimitive.java
+++ src/main/java/com/github/mavolin/maxon/jsonvalues/JsonPrimitive.java
@@ -1,35 +1,68 @@
 package com.github.mavolin.maxon.jsonvalues;
 
+/**
+ * {@code JsonPrimitive} represents all four JSON primitive types: Nulls, Booleans, Numbers and Strings. It acts like
+ * a wrapper through which the values can be extracted using their respective Java representation. Additionally this
+ * will provide for {@link char chars}, by which are represented as a one character JSON String.
+ */
 public class JsonPrimitive {
 
 
-    private Object value;
-
-    public JsonPrimitive(String string) {
-
-
-    }
-
-    public JsonPrimitive(Number number) {
+    /**
+     * The value of this {@code JsonPrimitive}. This is of one of the following classes: {@link Boolean Boolean},
+     * {@link Number Number} or {@link String String} or it is {@code null}.
+     */
+    private final Object value;
 
 
-    }
-
+    /**
+     * Instantiates a new {@code JsonPrimitive} holding a {@link Boolean Boolean} value.
+     *
+     * @param bool
+     *         the {@code Boolean Boolean}
+     */
     public JsonPrimitive(Boolean bool) {
 
-
+        this.value = bool;
     }
 
+    /**
+     * Instantiates a new {@code JsonPrimitive} holding {@link Character Character} value.
+     *
+     * @param character
+     *         the {@link Character Character}
+     */
     public JsonPrimitive(Character character) {
 
+        this.value = String.valueOf(character);
+    }
 
+    /**
+     * Instantiates a new {@code JsonPrimitive} holding a {@link Number Number} value.
+     *
+     * @param number
+     *         the number
+     */
+    public JsonPrimitive(Number number) {
+
+        this.value = number;
+    }
+
+    /**
+     * Instantiates a new {@code JsonPrimitive} holding a {@link String String} value.
+     *
+     * @param string
+     *         the {@link String String}
+     */
+    public JsonPrimitive(String string) {
+
+        this.value = string;
     }
 
 
     public boolean isNull() {
-
         throw new UnsupportedOperationException("Not implemented yet");
-    }
+   }
 
     public boolean isBoolean() {
 
