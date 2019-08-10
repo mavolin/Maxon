9310ff64d0fff020c9e694fe92820fd78adb3292
Maximilian von Lindern <48887425+mavolin@users.noreply.github.com>
2019-08-10 19:01:37 +0200

    added logic for getAsBoolean()

--- src/main/java/com/github/mavolin/maxon/jsonvalues/JsonElement.java
+++ src/main/java/com/github/mavolin/maxon/jsonvalues/JsonElement.java
@@ -1,15 +1,15 @@
 package com.github.mavolin.maxon.jsonvalues;
 
+import com.github.mavolin.maxon.exceptions.IllegalTypeRequestedException;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.function.BooleanSupplier;
 
 /**
  * A {@code JsonElement} is the Object used by {@link JsonArray JsonArrays} and {@link JsonObject JsonObjects} in their
  * respective collections, to make the use of the enhanced for loop more easy to use. To do so {@code JsonElement}
  * contains {@code getAsX()} methods to prevent the need of casting on the user end.
  */
-// TODO add reference to JsonParser
 public class JsonElement {
 
 
@@ -19,15 +19,37 @@ public class JsonElement {
     private JsonValue value;
 
 
+    /**
+     * Creates a new {@code JsonElement} holding the passed {@link JsonValue JsonValue}.
+     *
+     * @param value
+     *         the value
+     */
     public JsonElement(JsonValue value) {
 
         this.value = value;
     }
 
 
-    public BooleanSupplier getAsBoolean() {
+    /**
+     * Gets this {@code JsonElement} as a {@link Boolean Boolean}.
+     *
+     * @return the {@link Boolean Boolean}
+     *
+     * @throws IllegalTypeRequestedException
+     *         if this {@code JsonElement's} value is not a {@code Boolean}
+     */
+    public Boolean getAsBoolean() throws IllegalTypeRequestedException {
 
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (!(this.value instanceof JsonPrimitive))
+            throw new IllegalTypeRequestedException("The value of this JsonElement is not a Boolean");
+
+        JsonPrimitive bool = (JsonPrimitive) this.value;
+
+        if (!bool.isBoolean() && !bool.isNull())
+            throw new IllegalTypeRequestedException("The value of this JsonElement is not a Boolean");
+
+        return bool.getAsBoolean();
     }
 
     public Character getAsCharacter() {
@@ -78,6 +100,11 @@ public class JsonElement {
     public String getAsString() {
 
         throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public <T> T getAsObject(Class<T> clazz) {
+
+        throw new UnsupportedOperationException("Not implemented yet"); // TODO add
     }
 
 
