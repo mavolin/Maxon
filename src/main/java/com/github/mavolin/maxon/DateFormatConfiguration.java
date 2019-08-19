66444f1167c160fcad953fb0338eae8672fdc9e1
Maximilian von Lindern <48887425+mavolin@users.noreply.github.com>
2019-08-19 22:26:13 +0200

    added

---
+++ src/main/java/com/github/mavolin/maxon/DateFormatConfiguration.java
@@ -0,0 +1,108 @@
+package com.github.mavolin.maxon;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * The {@code DateFormatConfiguration} is used by {@link Maxon Maxon} to convert date and time objects of the new {@link
+ * java.time java.time} package to and from JSON.
+ */
+public class DateFormatConfiguration {
+
+
+    /**
+     * The {@link DateTimeFormatter DateTimeFormatter used for {@link java.time.LocalTime LocalTime} objects.}
+     */
+    DateTimeFormatter localTimeFormatter;
+    /**
+     * The {@link DateTimeFormatter DateTimeFormatter used for {@link java.time.LocalDate LocalDate} objects.}
+     */
+    DateTimeFormatter localDateFormatter;
+    /**
+     * The {@link DateTimeFormatter DateTimeFormatter used for {@link java.time.LocalDateTime LocalDateTime} objects.}
+     */
+    DateTimeFormatter localDateTimeFormatter;
+    /**
+     * The {@link DateTimeFormatter DateTimeFormatter used for {@link java.time.OffsetDateTime OffsetDateTime}
+     * objects.}
+     */
+    DateTimeFormatter offsetDateTimeFormatter;
+    /**
+     * The {@link DateTimeFormatter DateTimeFormatter used for {@link java.time.ZonedDateTime ZonedDateTime} objects.}
+     */
+    DateTimeFormatter zonedDateTimeFormatter;
+
+    /**
+     * Instantiates a new Date format configuration.
+     */
+    public DateFormatConfiguration() {
+
+
+    }
+
+    /**
+     * Sets the {@link DateTimeFormatter DateTimeFormatter} for {@link java.time.LocalTime LocalTime} to the passed one
+     * . If the passed {@link DateTimeFormatter DateTimeFormatter} is null, the default {@link DateTimeFormatter
+     * DateTimeFormatter} will be used.
+     *
+     * @param localTimeFormatter
+     *         the {@link DateTimeFormatter DateTimeFormatter}
+     */
+    public void setLocalTimeFormatter(DateTimeFormatter localTimeFormatter) {
+
+        this.localTimeFormatter = localTimeFormatter;
+    }
+
+    /**
+     * Sets the {@link DateTimeFormatter DateTimeFormatter} {@link java.time.LocalDate LocalDate} to the passed one. If
+     * the passed {@link DateTimeFormatter DateTimeFormatter} is null, the default {@link DateTimeFormatter
+     * DateTimeFormatter} will be used.
+     *
+     * @param localDateFormatter
+     *         the {@link DateTimeFormatter DateTimeFormatter}
+     */
+    public void setLocalDateFormatter(DateTimeFormatter localDateFormatter) {
+
+        this.localDateFormatter = localDateFormatter;
+    }
+
+    /**
+     * Sets the {@link DateTimeFormatter DateTimeFormatter} {@link java.time.LocalDateTime LocalDateTime} to the passed
+     * one. If the passed {@link DateTimeFormatter DateTimeFormatter} is null, the default {@link DateTimeFormatter
+     * DateTimeFormatter} will be used.
+     *
+     * @param localDateTimeFormatter
+     *         the {@link DateTimeFormatter DateTimeFormatter}
+     */
+    public void setLocalDateTimeFormatter(DateTimeFormatter localDateTimeFormatter) {
+
+        this.localDateTimeFormatter = localDateTimeFormatter;
+    }
+
+    /**
+     * Sets the {@link DateTimeFormatter DateTimeFormatter} {@link java.time.OffsetDateTime OffsetDateTime} to the
+     * passe* on . If the passed {@link DateTimeFormatter DateTimeFormatter} is null, the default {@link
+     * DateTimeFormatter DateTimeFormatter} will be used.
+     *
+     * @param offsetDateTimeFormatter
+     *         the {@link DateTimeFormatter DateTimeFormatter}
+     */
+    public void setOffsetDateTimeFormatter(DateTimeFormatter offsetDateTimeFormatter) {
+
+        this.offsetDateTimeFormatter = offsetDateTimeFormatter;
+    }
+
+    /**
+     * Sets the {@link DateTimeFormatter DateTimeFormatter} {@link java.time.ZonedDateTime ZonedDateTime} to the passed
+     * on . If the passed {@link DateTimeFormatter DateTimeFormatter} is null, the default {@link DateTimeFormatter
+     * DateTimeFormatter} will be used.
+     *
+     * @param zonedDateTimeFormatter
+     *         the {@link DateTimeFormatter DateTimeFormatter}
+     */
+    public void setZonedDateTimeFormatter(DateTimeFormatter zonedDateTimeFormatter) {
+
+        this.zonedDateTimeFormatter = zonedDateTimeFormatter;
+    }
+
+
+}
---
+++ src/main/java/com/github/mavolin/maxon/MaxonConfigurator.java
@@ -0,0 +1,141 @@
+package com.github.mavolin.maxon;
+
+import java.time.format.DateTimeFormatter;
+
+public class MaxonConfigurator {
+
+
+    /**
+     * The character used as whitespace when {@link PrintStyle#SINGLE_WHITESPACE PrintStyle.SINGLE_WHITESPACE} or
+     * {@link PrintStyle#PRETTY_PRINTED PrintStyle.PRETTY_PRINTED} are selected.
+     */
+    char whitespaceChar;
+    /**
+     * The quantity of whitespace characters used in {@link PrintStyle#PRETTY_PRINTED PrintStyle.PrettyPrinted} mode,
+     * as indent.
+     */
+    int whitespaceCharQty;
+    /**
+     * The style the JSON is being returned.
+     */
+    PrintStyle printStyle;
+    /**
+     * Defines if {@code nulls} found in {@link com.github.mavolin.maxon.jsonvalues.JsonArray JsonArrays} and
+     * {@link com.github.mavolin.maxon.jsonvalues.JsonObject JsonObjects} are to be serialized or not;
+     */
+    boolean ignoreNull;
+    /**
+     * Holds the {@link DateTimeFormatter DateTimeFormatter} for the various date and time object of
+     * {@link java.time java.time}
+     */
+    DateFormatConfiguration dateFormatConfiguration;
+
+
+    /**
+     * Instantiates a new {@code MaxonConfigurator}.
+     */
+    public MaxonConfigurator() {
+
+        this.whitespaceChar = ' ';
+        this.whitespaceCharQty = 4;
+        this.printStyle = PrintStyle.SINGLE_WHITESPACE;
+        this.ignoreNull = false;
+        this.dateFormatConfiguration = new DateFormatConfiguration();
+    }
+
+    /**
+     * <p>
+     *     If set to true, the default whitespace character will be a tabulator instead of a space.
+     * </p>
+     * <p>
+     *     The default setting is {@code false}
+     * </p>
+     *
+     * @param bool
+     *         {@code true} to use tabulator as whitespace character; {@code false} to use a space
+     */
+    public void toggleTabWhitespace(boolean bool) {
+
+        if (bool) {
+            this.whitespaceChar = '\t';
+        } else {
+            this.whitespaceChar = ' ';
+        }
+    }
+
+    /**
+     * <p>
+     *     Sets the quantity of whitespace characters used for one indent when in {@link PrintStyle#PRETTY_PRINTED
+     *     PrintStyle.PRETTY_PRINTED} mode.
+     * </p>
+     * <p>
+     *     The default setting is {@code 4}.
+     * </p>
+     *
+     * @param qty
+     *         the quantity of whitespace characters in one indent
+     */
+    public void setWhitespaceQuantity(int qty) {
+
+        this.whitespaceCharQty = qty;
+    }
+
+    /**
+     * <p>
+     *     Sets the {@link PrintStyle PrintStyle} used to generate JSON {@link String Strings} to the passed
+     *     {@link PrintStyle
+     *     PrintStyle}.
+     * </p>
+     * <p>
+     *     The default setting is {@link PrintStyle#SINGLE_WHITESPACE PrintStyle.SINGLE_WHITESPACE}.
+     * </p>
+     *
+     * @param printStyle
+     *         the {@link PrintStyle PrintStyle}
+     */
+    public void setPrintStyle(PrintStyle printStyle) {
+
+        this.printStyle = printStyle;
+    }
+
+    /**
+     * <p>
+     *     Choose whether or not {@code nulls} found in {@link com.github.mavolin.maxon.jsonvalues.JsonArray
+     *     JsonArrays} and {@link com.github.mavolin.maxon.jsonvalues.JsonObject JsonObjects} are to be ignored or not
+     * </p>
+     * <p>
+     *     The default setting is {@code false}.
+     * </p>
+     *
+     * @param ignoreNull
+     *         {@code true} if {@code nulls} should be ignored; {@code false} otherwise
+     */
+    public void setIgnoreNull(boolean ignoreNull) {
+
+        this.ignoreNull = ignoreNull;
+    }
+
+    /**
+     * <p>
+     *     Sets the {@link DateFormatConfiguration DateFormatConfiguration} which defines how date and time objects
+     *     shall be parsed.
+     * </p>
+     * <p>
+     *     The default value is {@code new DateFormatConfiguration}.
+     * </p>
+     *
+     * @param dateFormatConfiguration
+     *         the date format configuration
+     */
+    public void setDateFormatConfiguration(DateFormatConfiguration dateFormatConfiguration) {
+
+        this.dateFormatConfiguration = dateFormatConfiguration;
+    }
+
+    public Maxon buildMaxon() {
+
+        return new Maxon(this);
+    }
+
+
+}
