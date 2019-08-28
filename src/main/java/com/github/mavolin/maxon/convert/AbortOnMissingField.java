cbafa01d78e5d9f4811272a2b69973e80c00f63c
Maximilian von Lindern <48887425+mavolin@users.noreply.github.com>
2019-08-28 13:18:44 +0200

    added

---
+++ src/main/java/com/github/mavolin/maxon/convert/AbortOnMissingField.java
@@ -0,0 +1,27 @@
+package com.github.mavolin.maxon.convert;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to specify whether or not an exception should be thrown, in case a field is not present in
+ * a JSON, that is being deserialized. If a class is annotated with this annotation, the setting will be used
+ * globally and can only be overwritten by an annotated field. If the class itself is not annotated, the global
+ * setting will be set to {@code false} ignoring all missing field and using there default values.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD})
+public @interface AbortOnMissingField {
+
+
+    /**
+     * Defines whether or not to abort in case there is no mapping for a field.
+     *
+     * @return {@code true} if to abort; {@code false} otherwise
+     */
+    boolean value() default false;
+
+
+}
--- src/main/java/com/github/mavolin/maxon/converter/UniversalObjectConverter.java
+++ src/main/java/com/github/mavolin/maxon/converter/UniversalObjectConverter.java
@@ -1,6 +1,47 @@
 package com.github.mavolin.maxon.converter;
 
-public class UniversalObjectConverter {
+import com.github.mavolin.maxon.Maxon;
+import com.github.mavolin.maxon.convert.JsonConverter;
+import com.github.mavolin.maxon.jsonvalues.JsonValue;
+
+/**
+ * The {@code UniversalObjectConverter} is used to provide a way to convert all {@link Object Objects} by using
+ * Java's reflection api.
+ */
+public class UniversalObjectConverter implements JsonConverter {
+
+
+    /**
+     * Converts the passed {@link Object Object} to a {@link JsonValue JsonValue}, which is then processed further by
+     * {@link Maxon Maxon}.
+     *
+     * @param source
+     *         the {@link Object Object} that is to be converted
+     *
+     * @return the converted {@link Object Object}
+     */
+    @Override
+    public JsonValue getAsJson(Object source) {
+
+        return null;
+    }
+
+    /**
+     * Extracts information from the passed {@link JsonValue JsonValue} and builds a new {@link Object Object} of the
+     * type {@code T} out of it.
+     *
+     * @param source
+     *         the JSON representation of the {@link Object Object}
+     * @param clazz
+     *         the {@link Class Class} of the {@link Object Object}
+     *
+     * @return the extracted {@link Object Object}
+     */
+    @Override
+    public <T> T getFromJson(JsonValue source, Class<T> clazz) {
+
+        return null;
+    }
 
 
 }
